7f3faa8560633a1dbb53c969712bb9c10953d9af
Queean <378243518@qq.com>
2018-04-10 21:56:59 +0800

    first

---
+++ guess.py
@@ -0,0 +1,12 @@
+number = 55
+guess = int(input('enter an integer : '))
+
+if guess == number:
+    print('congratulations,you guessed it')
+    print('but you not win any prizes')
+elif guess<number:
+    print(' too small')
+else:
+    print('too higher')
+
+print('done')
