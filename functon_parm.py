c50ba9487ac4098b38bf989318862a0093093ae9
Queean <378243518@qq.com>
2018-04-14 11:43:26 +0800

    first

---
+++ functon_parm.py
@@ -0,0 +1,12 @@
+def print_max(a, b):
+    if a == b:
+        print(a, 'is equal to ', b)
+    elif a < b:
+        print(b, 'is maximum')
+    else:
+        print(a, 'is maximum')
+print_max(3, 4)
+
+x= 6
+y=7
+print_max(x, y)
